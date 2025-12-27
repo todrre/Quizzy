a7e9c931e4ea53d039c81dcad85e4041c414571a
Isac Persson <isac.persson@bredband2.com>
2025-12-27 19:37:53 +0100

    ändrat app.py

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import os
+
 from quizzy import create_app
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    host = os.environ.get("FLASK_HOST", "0.0.0.0")
+    port = int(os.environ.get("FLASK_PORT", "5000"))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host=host, port=port, debug=debug)
